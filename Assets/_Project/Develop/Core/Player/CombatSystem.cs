c74e2e1799588fb5ba0f282603a8d36610cf0c79
NeTBoyBot <toolumini@mail.ru>
2025-03-02 17:50:02 +0300

    Fix block

---
+++ Assets/_Project/Develop/Core/Player/CombatSystem.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using _Project.Develop.Core.Base;
+using _Project.Develop.Core.Enum;
+using UnityEngine;
+
+public class CombatSystem : MonoBehaviour
+{
+    private Creature playerModel;
+    [SerializeField]private Animator animator;
+
+    [SerializeField] private float baseMoveSpeed = 5f;
+    private float currentMoveSpeed;
+    [SerializeField] private float attackRange = 1.5f;
+    [SerializeField] private float attackCooldown = 0.5f;
+    [SerializeField] private float comboWindow = 0.5f; 
+    private float lastAttackTime;
+    private int currentAttackIndex = 0; 
+
+    [SerializeField] private bool hasShield = true; 
+    private bool isBlocking = false;               
+    private bool isBlockStarting = false;          
+    private float blockStartTime;
+
+    private Vector3 moveInput;
+
+    void Start()
+    {
+        playerModel = new Creature("player1");
+
+        currentMoveSpeed = baseMoveSpeed;
+    }
+
+    void Update()
+    {
+        playerModel.Update(Time.deltaTime);
+
+
+
+        if (currentAttackIndex > 0 && Time.time - lastAttackTime > comboWindow)
+        {
+            ResetAttackCombo();
+        }
+
+        if (Input.GetMouseButtonDown(0) && !isBlocking && Time.time - lastAttackTime >= attackCooldown)
+        {
+            PerformAttack();
+        }
+
+        if (hasShield)
+        {
+            if (Input.GetMouseButtonDown(1) && !isBlocking)
+            {
+                StartBlock();
+            }
+            else if (Input.GetMouseButtonUp(1) && isBlocking)
+            {
+                EndBlock();
+            }
+        }
+
+        if (isBlockStarting && Time.time - blockStartTime >= animator.GetCurrentAnimatorStateInfo(0).length/2)
+        {
+            isBlockStarting = false;
+            animator.SetBool("IsBlocking", true);
+        }
+
+        animator.SetBool("IsBlocking", isBlocking && !isBlockStarting);
+    }
+
+    private void PerformAttack()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("1H_Melee_Attack_Slice_Diagonal")
+            || animator.GetCurrentAnimatorStateInfo(0).IsName("1H_Melee_Attack_Stab")
+            || animator.GetCurrentAnimatorStateInfo(0).IsName("1H_Melee_Attack_Slice_Horizontal"))
+            return;
+        currentAttackIndex = Mathf.Min(currentAttackIndex + 1, 3);
+        lastAttackTime = Time.time;
+        
+
+        switch (currentAttackIndex)
+        {
+            case 1:
+                if (!animator.GetCurrentAnimatorStateInfo(0).IsName("1H_Melee_Attack_Slice_Horizontal") && !animator.GetCurrentAnimatorStateInfo(0).IsName("1H_Melee_Attack_Stab"))
+                    animator.SetTrigger("Attack1");
+                break;
+            case 2:
+                animator.SetTrigger("Attack2");
+                break;
+            case 3:
+                animator.SetTrigger("Attack3");
+                ResetAttackCombo(); 
+                break;
+        }
+
+        Vector3 attackDirection = transform.forward;
+        Vector3 attackPoint = transform.position + attackDirection * attackRange * 0.5f;
+
+        // Проверка попадания
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint, attackRange * 0.5f);
+        foreach (var hit in hitEnemies)
+        {
+            if (hit.CompareTag("Enemy"))
+            {
+                SkeletonAI enemy = hit.GetComponent<SkeletonAI>();
+                if (enemy != null)
+                {
+                    float damage = playerModel.Stats[StatType.Damage].CurrentValue;
+                    enemy.TakeDamage(damage);
+                    Debug.Log($"Player attacked {hit.name} for {damage} damage!");
+                }
+            }
+        }
+    }
+
+    private void ResetAttackCombo()
+    {
+        currentAttackIndex = 0;
+        animator.ResetTrigger("Attack1");
+        animator.ResetTrigger("Attack2");
+    }
+
+    private void StartBlock()
+    {
+        isBlocking = true;
+        isBlockStarting = true;
+        blockStartTime = Time.time;
+        animator.SetTrigger("BlockStart");
+    }
+
+    private void EndBlock()
+    {
+        isBlocking = false;
+        isBlockStarting = false;
+        animator.SetBool("IsBlocking", false);
+        animator.ResetTrigger("BlockStart");
+    }
+
+    public void OnHitWhileBlocking()
+    {
+        if (isBlocking)
+        {
+            animator.SetTrigger("BlockHit");
+            Debug.Log("Player blocked an attack!");
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 attackPoint = transform.position + transform.forward * attackRange * 0.5f;
+        Gizmos.DrawWireSphere(attackPoint, attackRange * 0.5f);
+    }
+
+    public void ApplyEffect(Effect effect)
+    {
+        playerModel.ApplyEffect(effect);
+    }
+}
---
+++ Assets/_Project/Develop/Core/Player/Player.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private PlayerCharacter _playerCharacter;
+
+    [SerializeField] private PlayerCamera _playerCamera;
+    [Space] 
+    [SerializeField] private CameraSpring _cameraSpring;
+    [SerializeField] private CameraLean _cameraLean;
+
+    [SerializeField] private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+        _playerCharacter.Initialize();
+        _playerCamera.Initialize(_playerCharacter.GetCameraTarget());
+        
+        _cameraSpring.Initialize();
+        _cameraLean.Initialize();
+        
+    }
+
+
+    // Update is called once per frame
+    private Transform obj;
+    void Update()
+    {
+        var deltaTime = Time.deltaTime;
+
+        var cameraInput = new CameraInput() { Look = new Vector2(Input.GetAxis("Mouse X"),Input.GetAxis("Mouse Y")) };
+        _playerCamera.UpdateRotation(cameraInput);
+
+        var characterInput = new CharacterInput()
+        {
+            Rotation = _playerCamera.transform.rotation,
+            Move     = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")),
+            Jump     = Input.GetKeyDown(KeyCode.Space),
+            JumpSustain = /*Input.GetKey(KeyCode.Space)*/ false,
+            Crouch = /*input.Crouch.WasPressedThisFrame()
+            ? CrouchInput.Toggle :*/
+                /*Input.GetKey(KeyCode.LeftControl) ? CrouchInput.Press:*/ CrouchInput.None
+            
+        };
+       
+        
+        
+        _playerCharacter.UpdateInput(characterInput);
+        _playerCharacter.UpdateBody(deltaTime);
+        animator.SetFloat("MoveX",Input.GetAxis("Vertical"));
+        
+
+#if UNITY_EDITOR
+        /*if (Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            _playerCharacter.Throw(-_playerCamera.transform.forward,2);
+        }*/
+
+        
+#endif
+        
+    }
+
+    private void LateUpdate()
+    {
+        var cameraTarget = _playerCharacter.GetCameraTarget();
+        var state = _playerCharacter.GetState();
+        
+        _playerCamera.UpdatePosition(cameraTarget);
+        _cameraSpring.UpdateSpring(Time.deltaTime,cameraTarget.up);
+        _cameraLean.UpdateLean(Time.deltaTime,state.Acceleration,cameraTarget.up);
+    }
+
+    private void OnDestroy()
+    {
+    }
+
+    public void Teleport(Vector3 pos)
+    {
+        _playerCharacter.SetPosition(pos);
+    }
+}
